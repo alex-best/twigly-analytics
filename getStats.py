ce5771e6f10569dd07c89aa609a62a5e1743820f
rtakkar <ghav.takkar@gmail.com>
2018-04-25 13:33:33 +0530

    all order times

--- getStats.py
+++ getStats.py
@@ -3634,6 +3634,98 @@ class LateDeliveryHandler(BaseHandler):
 		self.render("templates/simpletabletemplate.html", page_url="/latedeliveries", page_title="Twigly Late Orders Summary",table_title="Late Orders Summary - Prioirty orders over 60 mintues and Non Priority orders over 90 minutes - "+parsedstartdate.strftime("%Y-%m-%d")+" to "+displayenddate.strftime("%Y-%m-%d"),tableSort="[]", daterange=daterange, outputtable=outputtable, user=current_user)
 
 
+class AllOrderTimeHandler(BaseHandler):
+	@tornado.web.authenticated
+	def get(self):
+		current_user = self.get_current_user().decode()
+		if current_user not in ["admin"]:
+			self.redirect('/stats')
+
+		horizon = self.get_argument("horizon", None)
+		startdate = self.get_argument("startdate", None)
+		enddate = self.get_argument("enddate", None)
+		if startdate is None:
+			if horizon is None:
+				horizon = 1
+			else:
+				horizon = int(horizon)
+
+			parsedenddate = datetime.date.today() +  datetime.timedelta(days=1)
+
+			parsedstartdate = parsedenddate - datetime.timedelta(days=horizon)
+			daterange = [parsedstartdate.strftime("%Y-%m-%d")]
+			for c in range(horizon-1):
+				daterange.append((parsedstartdate + datetime.timedelta(days=(c+1))).strftime("%Y-%m-%d"))
+		
+		else:
+			parsedenddate = datetime.datetime.strptime(enddate, "%d/%m/%y").date()
+			parsedenddate = parsedenddate + datetime.timedelta(days=1)
+			parsedstartdate = datetime.datetime.strptime(startdate, "%d/%m/%y").date()
+			daterange = []
+			for c in range((parsedenddate - parsedstartdate).days):
+				daterange.append((parsedstartdate + datetime.timedelta(days=c)).strftime("%Y-%m-%d"))
+
+		statsengine = sqlalchemy.create_engine(statsengine_url)
+		statssession = scoped_session(sessionmaker(bind=statsengine))
+
+
+		thissql1 = "select date(o.date_add), dz.falls_under_gurantee, st.name, o.order_id, us.name, o.delivery_address, timediff(d.time_add,a.time_add), dbs.name from orders o left join delivery_zones dz on o.delivery_zone_id=dz.delivery_zone_id left join stores st on o.store_id = st.store_id left join users us on us.user_id=o.user_id left join deliveries dd on o.order_id=dd.order_id left join delivery_boys dbs on dd.delivery_boy_id=dbs.delivery_boy_id left join order_status_times as a on o.order_id = a.order_id left join order_status_times as d on o.order_id = d.order_id where a.order_status=1 and d.order_status=3 and o.order_status in (3,10,11,12,16) and o.date_add>='" + parsedstartdate.strftime("%Y-%m-%d") + " 00:00:00' and o.date_add <'" + parsedenddate.strftime("%Y-%m-%d") + " 00:00:00' order  by 1,3,8,2 desc,7 desc;"
+		result1 = statsengine.execute(thissql1)
+
+		result1backup = []
+		orderids = []
+		totaltimelookup = {}
+		for item in result1:
+			if (str(item[3]) not in orderids):
+				result1backup.append(item)
+				orderids.append(str(item[3]))
+				totaltimelookup[str(item[3])] = item[6]
+
+		outputtable = "<table class='table tablesorter table-striped table-hover'><thead><th>Date</th><th>Store</th><th>Delivery Boy</th><th>Priority</th><th>Order ID</th><th>Total Time Taken</th><th>Customer Name</th><th>Delivery Address</th><th>Cooking to Dispatch</th><th>Dispatch to Reached</th><th>Reached to Delivered</th></thead>"
+
+		prioritylookup = {0:"Non Priority", 1:"Priority"}
+
+		if(len(orderids) > 0):
+
+			cookingtodispatchsql = "select o.order_id, timediff(b.time_add,a.time_add) from orders o left join order_status_times as a on o.order_id = a.order_id left join order_status_times as b on o.order_id = b.order_id where a.order_status=1 and b.order_status=2 and o.order_id in ("+",".join(orderids)+");"
+
+			result2 = statsengine.execute(cookingtodispatchsql)
+			c2dlookup = {oid:0 for oid in orderids}
+			for item in result2:
+				c2dlookup[str(item[0])] = item[1]
+
+			dispatchtoreachedsql = "select o.order_id, timediff(b.time_add,a.time_add) from orders o left join order_status_times as a on o.order_id = a.order_id left join order_status_times as b on o.order_id = b.order_id where a.order_status=2 and b.order_status=15 and o.order_id in ("+",".join(orderids)+");"
+
+			result3 = statsengine.execute(dispatchtoreachedsql)
+			d2rlookup = {oid:0 for oid in orderids}
+			for item in result3:
+				d2rlookup[str(item[0])] = item[1]
+
+			reachedtodeliveredsql = "select o.order_id, timediff(b.time_add,a.time_add) from orders o left join order_status_times as a on o.order_id = a.order_id left join order_status_times as b on o.order_id = b.order_id where a.order_status=15 and b.order_status=3 and o.order_id in ("+",".join(orderids)+");"
+
+			result4 = statsengine.execute(reachedtodeliveredsql)
+			r2dlookup = {oid:0 for oid in orderids}
+			for item in result4:
+				r2dlookup[str(item[0])] = item[1]
+
+			for item in result1backup:
+				key1 = str(item[3])
+				outputtable += "<tr><td>" + str(item[0]) + "</td><td>"+str(item[2])+"</td><td>"+str(item[7])+"</td><td>" 
+				if item[1] in prioritylookup:
+					outputtable += prioritylookup[item[1]]
+				outputtable += "</td><td><a href='http://twigly.in/admin/orders?f="+str(item[3])+"'>"+str(item[3])+"</a></td><td>"+str(totaltimelookup[key1])+"</td><td>"+str(item[4])+"</td><td>"+str(item[5])+"</td><td>"+str(c2dlookup[key1])+"</td><td>"+str(d2rlookup[key1])+"</td><td>"+str(r2dlookup[key1])+"</td></tr>"
+
+		else:
+			outputtable += "<tr><td colspan=12 style='text-align: left !important;'>No orders in this time frame. Adjust the date range to see order delays.</td></tr>"
+
+		outputtable += "</table>"
+
+		displayenddate = parsedenddate +  datetime.timedelta(days=-1)
+
+		statssession.remove()
+		self.render("templates/simpletabletemplate.html", page_url="/alldeliveries", page_title="Twigly All Orders Time Summary",table_title="All Order Times - "+parsedstartdate.strftime("%Y-%m-%d")+" to "+displayenddate.strftime("%Y-%m-%d"),tableSort="[]", daterange=daterange, outputtable=outputtable, user=current_user)
+
+
 
 class DeliveryStatsHandler(BaseHandler):
 	@tornado.web.authenticated
@@ -5066,6 +5158,7 @@ application = tornado.web.Application([
 	(r"/setdatewisestoremenu", SetDateWiseMenuHandler),
 	(r"/deliveries", DeliveryHandler),
 	(r"/latedeliveries", LateDeliveryHandler),
+	(r"/alldeliveries", AllOrderTimeHandler),
 	(r"/deliverystats", DeliveryStatsHandler),
 	(r"/payments", PaymentStatsHandler),
 	(r"/orderstats", OrderStatsHandler),
